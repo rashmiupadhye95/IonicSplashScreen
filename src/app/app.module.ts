50a96e4914f3a8b0f4baa26555e6f38b199cd54b
Rashmi Upadhye <rashmi11395@gmail.com>
2019-05-14 14:45:31 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,33 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: "AIzaSyCIazzEbtRYaIFTNSuiAkvsybFiEE8SVPU",
+  authDomain: "ionic-push-project-4fb4e.firebaseapp.com",
+  databaseURL: "https://ionic-push-project-4fb4e.firebaseio.com",
+  projectId: "ionic-push-project-4fb4e",
+  storageBucket: "ionic-push-project-4fb4e.appspot.com",
+  messagingSenderId: "1034780571543"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
